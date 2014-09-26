daeeccb9f525db3599985361abd62e37ae674113
alfredo <alfredo.e.alvarez@gmail.com>
2014-09-25 23:15:47 -0400

    Added functionality to all the forms missing to add the proof read text.

--- ArrancaPr/arrancapr/__init__.py
+++ ArrancaPr/arrancapr/__init__.py
@@ -1,6 +1,14 @@
 from pyramid.config import Configurator
 
+try:
+    # for python 2
+    from urlparse import urlparse
+except ImportError:
+    # for python 3
+    from urllib.parse import urlparse
 
+import pymongo
+from pymongo import MongoClient
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
@@ -12,4 +20,12 @@ def main(global_config, **settings):
     config.add_route('project', '/project')
     config.add_route('contact', '/contact')
     config.scan()
+
+    db_url = urlparse(settings['mongo_uri'])
+    config.registry.db = MongoClient(settings['mongo_uri'])
+
+    def get_db(request):
+        db = config.registry.db[db_url.path[1:]]
+        return db
+    config.add_request_method(get_db, 'db', reify=True)
     return config.make_wsgi_app()
--- ArrancaPr/arrancapr/views.py
+++ ArrancaPr/arrancapr/views.py
@@ -9,16 +9,42 @@ def site_layout():
 
 @view_config(route_name='home', renderer='templates/index.pt')
 def my_view(request):
+    if "name" in request.params:
+        personName = request.params["name"]
+        personEmail = request.params["email"]
+        post = {"name":personName,
+                "email":personEmail}
+
+        request.db['MemberRegistration'].insert(post,safe=True)
     return {'project': 'ArrancaPr', "layout": site_layout()}
 
 @view_config(route_name='about', renderer='templates/about.pt')
 def about_view(request):
+
     return {'project': 'ArrancaPr', "layout": site_layout()}
 
 @view_config(route_name='contact', renderer='templates/contact.pt')
 def contact_view(request):
+    if "name" in request.params:
+        personName = request.params["name"]
+        personEmail = request.params["email"]
+        telephone = request.params["tel"]
+        body = request.params["body"]
+        post = {"name":personName,
+                "email":personEmail,
+                "telephone":telephone,
+                "body":body}
+        request.db['CustomerComments'].insert(post,safe=True)
     return {'project': 'ArrancaPr', "layout": site_layout()}
 
 @view_config(route_name='project', renderer='templates/project.pt')
 def project_view(request):
-    return {'project': 'ArrancaPr', "layout": site_layout()}
+     if "name" in request.params:
+        personName = request.params["name"]
+        personEmail = request.params["email"]
+        project = request.params["project"]
+        post = {"name":personName,
+                "email":personEmail,
+                "project":project}
+        request.db['ClassRegistration'].insert(post,safe=True)
+     return {'project': 'ArrancaPr', "layout": site_layout()}
