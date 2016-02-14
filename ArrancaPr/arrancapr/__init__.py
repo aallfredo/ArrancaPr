de474fd8e9243b3d8d3293a28a67c767a7a5eb92
Edwin Martinez <tusoftwarepersonal@gmail.com>
2016-02-14 00:29:22 -0400

    Calendar added

--- ArrancaPr/arrancapr/__init__.py
+++ ArrancaPr/arrancapr/__init__.py
@@ -18,6 +18,7 @@ def main(global_config, **settings):
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('home', '/')
     config.add_route('about', '/about')
+    config.add_route('calendar', '/calendar')
     config.add_route('project', '/project')
     config.add_route('media', '/media')
     config.add_route('hackerspace', '/hackerspace')
